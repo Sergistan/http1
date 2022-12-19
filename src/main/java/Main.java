4a44c5343d0bf5e1bf846f063f43b62dc6b3b5d3
Sergei Utochkin <dzaga73i98@gmail.com>
2022-12-19 20:27:32 +0300

    http1

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    final static String URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+        HttpGet request = new HttpGet(URL);
+        CloseableHttpResponse response = createHttpClient().execute(request);
+        List<JsonClass> posts = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<JsonClass>>() {});
+        posts.stream().filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0).forEach(System.out::println);
+    }
+
+    public static CloseableHttpClient createHttpClient() {
+        return HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+    }
+}
+
